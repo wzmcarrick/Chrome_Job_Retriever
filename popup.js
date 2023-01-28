a9e7e248b2dfc3dc380858a1383c81ea875cb9b7
wzmcarrick <wzmcarrick@gmail.com>
2023-01-28 17:22:27 -0500

    support LinkedIn and Glassdoor

--- popup.js
+++ popup.js
@@ -11,22 +11,34 @@ async function getCurrentTab() {
 // OnClick function for the button, jquery and our customized code will be injected and executed
 // TODO: jquery was injected and parsed every time we click the button and can use a lot of resource,
 // Can we inject jquery only once?
+let tabVisited = [];
 
 retrieveJob.addEventListener('click', async () => {
     console.log("retrieveJob click called")
     let tab = await getCurrentTab();
     console.log(tab)
 
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: [
-            './scripts/jquery-3.6.3.min.js',
-            './scripts/content-script.js'
-        ]
-    },
-        res => {
-            console.log("get jobDetails from current-tab", res[0].result)
+    if (tabVisited.includes(tab.id)) {
+        console.log('this tab is visited')
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['./scripts/content-script.js']
+        })
+    } else {
+        console.log('this tab is not visited')
+        tabVisited.push(tab.id);
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: [
+                './scripts/jquery-3.6.3.min.js',
+                './scripts/content-script.js'
+            ],
+        },
+            res => {
+                console.log("get jobDetails from current-tab", res[0].result)
 
-            // TODO: send API call to notion
-        });
+                // TODO: send API call to notion
+            });
+    }
+
 });
--- scripts/content-script.js
+++ scripts/content-script.js
@@ -5,13 +5,34 @@ var $ = jQuery;
 
 function retrieveJob() {
     console.log("retrieveJob() called")
+    let jobtitle, jobDescription
 
-    // TODO: need to use chrome API to find the website name, for now I assume the website is ca.indeed.com
+    loc = window.location.toString()
+    console.log(loc)
 
-    const jobtitle = $(".jobsearch-JobInfoHeader-title").text() // get job title
-    const jobDescription = $("#jobDescriptionText").text() // get job description as plain text
+    if (loc.includes('ca.indeed.com')) {
+        console.log('this is an indeed page')
+        jobtitle = $(".jobsearch-JobInfoHeader-title").text() // get job title
+        jobDescription = $("#jobDescriptionText").text() // get job description as plain text
 
-    console.log("retrieved job title and description from current-tab", jobtitle, jobDescription)
+        console.log("retrieved job title and description from indeed", jobtitle, jobDescription)
+
+
+    } else if (loc.includes('linkedin.com')) {
+        console.log('this is a linkedin page')
+        jobtitle = $(".jobs-unified-top-card__job-title").text() // get job title
+        jobDescription = $(".jobs-description__content").text() // get job description as plain text
+
+        console.log("retrieved job title and description from LinkedIn", jobtitle, jobDescription)
+
+
+    } else if (loc.includes('glassdoor.ca')) {
+        console.log('this is a glassdoor page')
+        jobtitle = $(".e1tk4kwz4").text() // get job title
+        jobDescription = $("#JobDescriptionContainer").text() // get job description as plain text
+
+        console.log("retrieved job title and description from glassdoor", jobtitle, jobDescription)
+    }
 
     // return data to extension
     return {
